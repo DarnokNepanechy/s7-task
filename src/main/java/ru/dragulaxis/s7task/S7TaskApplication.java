b38e0e5c14baca52776e119401cbdd3e5073e5d4
dragulaxis <dushesgt@gmail.com>
2022-02-02 09:50:55 +0300

    add all files

---
+++ src/main/java/ru/dragulaxis/s7task/S7TaskApplication.java
@@ -0,0 +1,13 @@
+package ru.dragulaxis.s7task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S7TaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dragulaxis/s7task/S7TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dragulaxis.s7task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S7TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
